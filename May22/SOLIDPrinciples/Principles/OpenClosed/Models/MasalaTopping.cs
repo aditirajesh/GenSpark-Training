e1ce435e4de4408e9b9e3c83e5aaa2eadc4447f1
Rajesh <arajesh@presidio.com>
2025-05-23 09:31:54 +0530

    application applying SOLID principles

---
+++ May22/SOLIDPrinciples/Principles/OpenClosed/Models/MasalaTopping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SOLIDPrinciples.Principles.OpenClosed.Interfaces;
+
+
+namespace SOLIDPrinciples.Principles.OpenClosed.Models
+{
+    public class MasalaTopping: ITopping
+    {
+        public string topping_name => "Masala";
+
+        public void Apply()
+        {
+            Console.WriteLine("A mix of Spicy Hawaiian Barbeque, Cheddar Cheese and Sour Cream Onion is sprinkled on the popcorn");
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ May22/SOLIDPrinciples/Program.cs
@@ -0,0 +1,17 @@
+﻿using SOLIDPrinciples.Principles.OpenClosed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrinciples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GoodExample1.Run();
+        }
+    }
+}
